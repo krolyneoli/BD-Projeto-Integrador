c186dffa3f0ed6220739ea19898b92d8649230ea
Gabriel Gomes <gabrielgomesduarte020@gmail.com>
2026-08-31 20:45:16 -0300

    feat: insert de adm/categoria

---
+++ adm/categoria/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO adm.categoria (nome) VALUES
+    ('Pães'),
+    ('Bolos'),
+    ('Doces'),
+    ('Salgados'),
+    ('Bebidas'),
+    ('Cafés e Chás'),
+    ('Tortas'),
+    ('Biscoitos e Cookies');
