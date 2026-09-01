3c2bce0d0103e1ec4658babfa90e42a4382beb13
CamillyBraga <aluno.camilly.braga@doctum.edu.br>
2026-09-01 20:55:26 -0300

    Update insert.sql

---
+++ site/carrinho/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO site.carrinho (id_usuario, id_cupom) VALUES
+    (1, 1),
+    (2, NULL),
+    (3, 3),
+    (4, NULL),
+    (5, 2),
+    (6, NULL),
+    (7, 5),
+    (8, NULL),
+    (9, NULL),
+    (10, NULL);
