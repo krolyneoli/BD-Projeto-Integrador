5898b6ce94a962cc67deb902c443bbeaa95c320a
Karolyne Oliveira <paxiegakarolyne@gmail.com>
2026-08-31 22:18:10 -0300

    feat: insert contabil.plano_contas para testes

--- INSERT_COMPLETO.sql
+++ INSERT_COMPLETO.sql
@@ -116,7 +116,11 @@ INSERT INTO adm.nota_fiscal (numero_nota, valor, id_venda) VALUES
     ('000000005', 315.00, 5);
 
 
-
+INSERT INTO contabil.plano_contas
+(codigo, nome_conta, tipo_conta, natureza_conta)
+VALUES
+('1.1', 'Caixa/Banco', 'ativo', 'devedora'),
+('4.1', 'Receita de Vendas', 'receita', 'credora');
 
 
 -- =========================================================
