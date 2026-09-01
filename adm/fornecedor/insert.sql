1472f5bcbe830df4c11bfcda0999571c71451f5b
Gabriel Gomes <gabrielgomesduarte020@gmail.com>
2026-08-31 21:04:20 -0300

    feat: inset de adm/produto_fornecedor/fornecedor

---
+++ adm/fornecedor/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO adm.fornecedor (nome, nome_social, nome_fantasia, email, tel, documento, id_endereco) VALUES
+    ('Distribuidora de Farinhas Ltda', NULL, 'FarinhasBR', 'contato@farinhasbr.com', '(35) 3435-1000', '12.345.678/0001-90', 1),
+    ('Laticínios Vale Verde', NULL, 'Vale Verde', 'vendas@valeverde.com', '(35) 3435-2000', '23.456.789/0001-01', 2),
+    ('Chocolates & Cia', NULL, 'ChocoCia', 'comercial@chococia.com', '(35) 3435-3000', '34.567.890/0001-12', 3),
+    ('Embalagens Sul Minas', NULL, 'Sul Minas Embalagens', 'contato@sulminas.com', '(35) 3435-4000', '45.678.901/0001-23', 4);
---
+++ adm/produto_fornecedor/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO adm.produto_fornecedor (id_produto, id_fornecedor) VALUES
+    (1, 1), 
+    (2, 1),  
+    (2, 2),  
+    (3, 2),  
+    (3, 3),  
+    (4, 3), 
+    (7, 3),  
+    (8, 3);  
