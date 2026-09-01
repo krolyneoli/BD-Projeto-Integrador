7f85e71a9a4a738b7eb4105cd41c89eefbbf7b83
Gabriel Gomes <gabrielgomesduarte020@gmail.com>
2026-08-31 21:01:45 -0300

    feat: insert de site/endereco

---
+++ site/endereco/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO site.endereco (rua, numero, bairro, cidade, estado, cep, complemento) VALUES
+    ('Rua das Flores', 120, 'Centro', 'Extrema', 'Minas Gerais', '37640-000', 'Apto 302'),
+    ('Avenida Brasil', 45, 'Jardim América', 'Extrema', 'Minas Gerais', '37640-010', NULL),
+    ('Rua São José', 88, 'Vila Nova', 'Camanducaia', 'Minas Gerais', '37650-000', 'Casa 2'),
+    ('Rua XV de Novembro', 250, 'Centro', 'Itapeva', 'São Paulo', '18400-000', NULL),
+    ('Rua das Palmeiras', 15, 'Bela Vista', 'Extrema', 'Minas Gerais', '37640-020', 'Fundos');
