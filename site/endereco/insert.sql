93381130cbc9ee439ec5045fc477828190725071
CamillyBraga <aluno.camilly.braga@doctum.edu.br>
2026-09-01 20:41:47 -0300

    Update insert.sql

--- site/endereco/insert.sql
+++ site/endereco/insert.sql
@@ -4,3 +4,8 @@ INSERT INTO site.endereco (rua, numero, bairro, cidade, estado, cep, complemento
     ('Rua São José', 89, 'Vila Nova', 'Camanducaia', 'MG', '37650-000', 'Casa 2'),
     ('Rua XV de Novembro', 500, 'Centro', 'Itapeva', 'SP', '18400-000', NULL),
     ('Rua das Palmeiras', 10, 'Bela Vista', 'Extrema', 'RJ', '37640-020', 'Fundos');
+    ('Rua das Acácias', 125, 'Centro', 'Extrema', 'MG', '37640-030', NULL),
+    ('Avenida Minas Gerais', 780, 'Jardim Europa', 'Extrema', 'MG', '37640-040', 'Apto 101'),
+    ('Rua Tiradentes', 45, 'Centro', 'Camanducaia', 'MG', '37650-010', NULL),
+    ('Rua São Paulo', 320, 'Vila Industrial', 'Itapeva', 'SP', '18400-020', 'Casa 1'),
+    ('Rua dos Ipês', 88, 'Bela Vista', 'Extrema', 'MG', '37640-050', 'Fundos');
